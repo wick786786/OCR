006c7d1a8e38390c6c297eace8e178261b320f40
Satyam Yadav <86571155+wick786786@users.noreply.github.com>
2023-12-25 23:49:58 +0530

    Update EditForm.js

--- client/src/Components/EditForm.js
+++ client/src/Components/EditForm.js
@@ -1,41 +1,40 @@
 import React from "react";
-import './Edit.css';
-
+import'./Edit.css';
 const EditForm = ({ data, onSubmit }) => {
   return (
-    <form onSubmit={onSubmit}>
-      <label>
-        First Name:
+    <form onSubmit={onSubmit} className="edit-form-container">
+      <div className="form-group">
+        <label>First Name:</label>
         <input type="text" name="firstName" defaultValue={data.firstName} />
-      </label>
-      <label>
-        Last Name:
+      </div>
+      <div className="form-group">
+        <label>Last Name:</label>
         <input type="text" name="lastName" defaultValue={data.lastName} />
-      </label>
-      <label>
-        Date of Birth:
+      </div>
+      <div className="form-group">
+        <label>Date of Birth:</label>
         <input
           type="text"
           name="dob"
           defaultValue={new Date(data.dateOfBirth).toLocaleDateString()}
         />
-      </label>
-      <label>
-        Date of Issue:
+      </div>
+      <div className="form-group">
+        <label>Date of Issue:</label>
         <input
           type="text"
           name="doi"
           defaultValue={new Date(data.issueDate).toLocaleDateString()}
         />
-      </label>
-      <label>
-        Date of Expiry:
+      </div>
+      <div className="form-group">
+        <label>Date of Expiry:</label>
         <input
           type="text"
           name="doe"
           defaultValue={new Date(data.expiryDate).toLocaleDateString()}
         />
-      </label>
+      </div>
       <button type="submit">Save</button>
     </form>
   );
